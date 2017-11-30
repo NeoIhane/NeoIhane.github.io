518fe7ec18358cbc03bba7e037ebfc89ab86d4b0
neoihane <orrumpha.wongsripaisan@gmail.com>
2017-12-01 01:00:18 +0700

    test

---
+++ Assets/Scripts/WaterControls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterControls : MonoBehaviour {
+
+    public Renderer rend;
+    void Start()
+    {
+        //rend = GetComponent<Renderer>();
+        //rend.material.shader = Shader.Find("Alpha");
+    }
+    public void SetAlpha(float alpha)
+    {
+        //renderer.material.SetFloat("_Alpha", alpha);
+        rend.material.SetFloat("_Alpha", alpha);
+
+    }
+}
